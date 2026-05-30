777f9da9c8ffb0da11984e540fc1056cfaab4618
megazzx <mega.slm2009@gmail.com>
2026-05-30 17:44:40 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,59 @@
+#check even or odd
+'''num = int(input("enter a number:"))
+if num % 2 ==0:
+    print("the number is even")
+else:
+    print("the number is odd")'''
+
+# positive,negative or zero
+'''num = int(input("enter a number:"))
+if num > 0:
+    print("the number is positive")
+elif num < 0:
+    print("the number is negative")
+else:
+    print("the number is zero")'''
+
+# divide by both 2 and 5
+'''num = int(input("enter a number:"))
+if num % 2 == 0 and num % 5 == 0:
+    print("the is divisible by both")
+else:
+    print("the number is not divisible by both")'''
+
+# Input marks and find grade
+'''mark = int(input("Enter mark: "))
+if mark >= 90:
+2034
+print("A Grade")
+elif mark >= 75:
+    print("B Grade")
+elif mark >= 45:
+    print("C Grade")
+else:
+    print("Fail")'''
+
+# Leap year using nested conditions
+'''year = int(input("Enter year: "))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap Year")
+        else:
+            print("Not a Leap Year")
+    else:
+        print("Leap Year")
+else:
+    print("Not a Leap Year")'''
+# PRINT 1 TO 10
+'''for i in range(1,11):
+    print(i)'''
+
+# square of 1 to 10
+'''for i in range(1,11):
+ square = i*i
+ print("square=",square)'''
+
+# print 10 to 1
+'''for i in range(10,0,-1):
+    print(i)'''
