52ce681d747952be6a1c9cf60ede6c02cec90845
megazzx <mega.slm2009@gmail.com>
2026-05-30 18:50:00 +0530

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,44 @@
+#check how many times the ch is printed
+'''a="malayalam"
+print(a.count("a"))'''
+
+#count vowels
+'''a= input("enter a word:")
+count =0
+for i in a:
+    if i in "AEIOUaeiou":
+        count+=1
+print("no of vowels:",count)'''
+
+#uppercase
+'''a ="hello world"
+b =a.upper().split()
+print(b)
+for i in b:
+    print(i)'''
+#reverse
+'''a = input("enter a word:")
+b =(a[::-1])
+print(b)
+count =0
+for i in a:
+    if i in "AEIOUaeiou":
+        count+=1
+print("no of vowels:",count)'''
+
+#palindrome
+'''a = input("enter a word:")
+b=(a[::-1])
+if a == b:
+    print("palindrome")
+else:
+    print("not a palindrome")'''
+# longest word
+'''a= input("enter a word:")
+b =a.split()
+print(b)
+for i in b:
+    if len(i)>=4:
+        print(i)'''
+
+        
